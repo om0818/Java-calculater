f455e6b07354c7f88f930a8d43c368f30fd3bbbf
om0818 <115542900+om0818@users.noreply.github.com>
2022-10-13 11:07:07 +0900

    Update CalcService.java
    足し算、引き算、掛け算、割り算の数字以外の文字が入力された場合のエラーを表示にするように変更しました。

--- WebCalc02Q/src/main/java/com/example/demo/services/CalcService.java
+++ WebCalc02Q/src/main/java/com/example/demo/services/CalcService.java
@@ -7,79 +7,96 @@ import org.springframework.stereotype.Component;
 @Component
 public class CalcService {
 
-	public String calculateAdd(
-			String NumA,
-			String NumB
-
-
-			)
+	public String calculateAdd(String NumA,String NumB)
 
 	{
-		BigDecimal AdA = new BigDecimal(NumA);
-		BigDecimal AdB = new BigDecimal(NumB);
-		BigDecimal AdR = AdA.add(AdB);
+		String AdResult = null;
+		BigDecimal AdA = null;
+		BigDecimal AdB = null;
+		try {
+			AdA = new BigDecimal(NumA);
+			AdB = new BigDecimal(NumB);
+			BigDecimal AdR = AdA.add(AdB);
+			AdResult = AdR.toString();
+			//数字以外が入った時がキャッチ
+		} catch (Exception e) {
 
-
-
-
-		String AdResult = AdR.toString();
+			AdResult = "数字を入力してください";
+		}
+		//トライ、キャッチのどっちかで抜けたとき
 		return AdResult;
+
 
 	}
 	/**
 	 * 引き算
 	 */
-	public String subtract(
-			String NumC,
-			String NumD
-			)
+	public String subtract(String NumC,String NumD)
 	{
-		BigDecimal AdA = new BigDecimal(NumC);
-		BigDecimal AdB = new BigDecimal(NumD);
-		BigDecimal AdR = AdA.subtract(AdB);
+		String AdResult =null;
+		BigDecimal AdA = null; 
+		BigDecimal AdB = null;
+		try {
+			AdA = new BigDecimal(NumC);
+			AdB = new BigDecimal(NumD);
+			BigDecimal AdR = AdA.subtract(AdB);
+			AdResult = AdR.toString();
 
+			//数字以外が入った時がキャッチ
+		} catch (Exception e) {
 
+			AdResult = "数字を入力してください";
 
-
-		String AdResult = AdR.toString();
+		}
+		//トライ、キャッチのどっちかで抜けたとき
 		return AdResult;
 	}
 	/*
 	 * 掛け算
 	 */
-	public String multiply (
-			String NumE,
-			String NumF
-			)
+	public String multiply (String NumE,String NumF)
 	{
-		BigDecimal AdA = new BigDecimal(NumE);
-		BigDecimal AdB = new BigDecimal(NumF);
-		BigDecimal AdR = AdA.multiply(AdB);
+		String AdResult = null;
+		BigDecimal AdA = null;
+		BigDecimal AdB = null;
+		try {
+			AdA = new BigDecimal(NumE);
+			AdB = new BigDecimal(NumF);
+			BigDecimal AdR = AdA.multiply(AdB);
+			AdResult = AdR.toString();
 
+		} catch (Exception e) {
 
+			AdResult = "数字を入力してください";
 
+		}
 
-		String AdResult = AdR.toString();
 		return AdResult;
 	}
 	/**
 	 * 割り算
 	 */
-	public String divide (
-			String NumG,
-			String NumH
-			)
+	public String divide (String NumG,String NumH)
 	{
-		BigDecimal AdA = new BigDecimal(NumG);
-		BigDecimal AdB = new BigDecimal(NumH);
+		String AdResult = null;
+		BigDecimal AdA = null;
+		BigDecimal AdB = null;
 		try {
+		AdA = new BigDecimal(NumG);
+		AdB = new BigDecimal(NumH);
+		
 			BigDecimal AdR = AdA.divide(AdB);
-			String AdResult = AdR.toString();
-			return AdResult;
+			AdResult = AdR.toString();
+
+
 		} catch (Exception e) {
-			return "無限小数の答えになりました";
+			AdResult = "数字を入力してください";
 		}
 
+		return AdResult;
+
+		//return "無限小数の答えになりました";
+	}
 
 
 
@@ -87,25 +104,26 @@ public class CalcService {
 
 
 
-		//		割り算に関しては特殊なので注意
-		//		
-		//		ヒントとしてtry、catch構文も記載はしておきます。
-		//		
-		//		後は調べてね！
 
-		//		try {
-		//			BigDecimal AdR = 〇〇.〇〇(AdB);
-		//			return AdR.toString();
-		//		} catch (Exception e) {
-		//			return "無限小数の答えになりました";
-		//		}
+	//		割り算に関しては特殊なので注意
+	//		
+	//		ヒントとしてtry、catch構文も記載はしておきます。
+	//		
+	//		後は調べてね！
 
-		/*このコメントアウトは消さない方がいい
-		 * これは無限少数の桁を設定する際に必要
+	//		try {
+	//			BigDecimal AdR = 〇〇.〇〇(AdB);
+	//			return AdR.toString();
+	//		} catch (Exception e) {
+	//			return "無限小数の答えになりました";
+	//		}
+
+	/*このコメントアウトは消さない方がいい
+	 * これは無限少数の桁を設定する際に必要
 	BigDecimal AdR = AdA.divide(AdB,7,RoundingMode.HALF_UP); //BigDecimal 〇〇= 〇〇.add(〇〇); これは足し算を書くとき
 	String AdResult = AdR.toString();//toStringはクラスで定義されている文字列表現を返すメソッド
 	return AdResult;
-		 */
+	 */
 
-	}
 }
+
